ea4be0b1341793ae5712d8c50c4c3d1ae817516e
massihchitsaz <massih.chitsaz@gmail.com>
2026-08-15 01:59:52 +0400

    Connect payables route to operational workspace

--- app/finance/payables/page.tsx
+++ app/finance/payables/page.tsx
@@ -1,2 +1,2 @@
-import { FinanceRegister } from "@/components/finance/FinanceRegister";
-export default function PayablesPage(){ return <FinanceRegister kind="payable"/>; }
+import { PayablesWorkspace } from "@/components/finance/PayablesWorkspace";
+export default function PayablesPage(){ return <PayablesWorkspace/>; }
