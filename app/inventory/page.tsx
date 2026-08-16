08eca0faab5884a544b6848bce3838adf1fe74a6
massihchitsaz <massih.chitsaz@gmail.com>
2026-08-16 12:22:32 +0400

    Integrate warehouse operations control into inventory page

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,9 +1,11 @@
 import { InventoryCommandCenter } from "@/components/inventory/InventoryCommandCenter";
 import { MultiWarehouseControl } from "@/components/inventory/MultiWarehouseControl";
+import { WarehouseOperationsControl } from "@/components/inventory/WarehouseOperationsControl";
 
 export default function InventoryPage(){
   return <>
     <MultiWarehouseControl/>
+    <WarehouseOperationsControl/>
     <InventoryCommandCenter/>
   </>;
 }
