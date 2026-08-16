b0006caa8854740185979bea16d0907c28be8be1
massihchitsaz <massih.chitsaz@gmail.com>
2026-08-16 13:30:58 +0400

    Integrate inventory intelligence into production workspace

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,10 +1,12 @@
 import { InventoryCommandCenter } from "@/components/inventory/InventoryCommandCenter";
 import { MultiWarehouseControl } from "@/components/inventory/MultiWarehouseControl";
 import { WarehouseOperationsControl } from "@/components/inventory/WarehouseOperationsControl";
+import { InventoryIntelligenceControl } from "@/components/inventory/InventoryIntelligenceControl";
 
 export default function InventoryPage(){
   return <>
     <MultiWarehouseControl/>
+    <InventoryIntelligenceControl/>
     <WarehouseOperationsControl/>
     <InventoryCommandCenter/>
   </>;
