586ed91d9b5cbf3864ded5474d78e1886f4b50bb
massihchitsaz <massih.chitsaz@gmail.com>
2026-08-17 08:50:29 +0400

    Harden deal trigger execution privileges

---
+++ supabase/migrations/20260817_deal_rpc_execution_hardening_v34.sql
@@ -0,0 +1,4 @@
+revoke all on function public.deal_creation_guard() from public,anon;
+revoke all on function public.deal_sensitive_update_guard() from public,anon;
+revoke all on function public.deal_commercial_lock_guard() from public,anon;
+revoke all on function public.deal_creation_integrity_guard() from public,anon;
